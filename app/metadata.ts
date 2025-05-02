4606ac3527fe03e165e41e79b3b6501e95995e0f
KIMMINGYU99 <kmk4604@gmail.com>
2025-05-02 21:45:31 +0900

    프론트

--- app/metadata.ts
+++ app/metadata.ts
@@ -3,4 +3,12 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "김민규 | 포트폴리오",
   description: "신입 프론트엔드 개발자 김민규의 포트폴리오입니다.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "김민규 | 포트폴리오",
+    description: "신입 프론트엔드 개발자 김민규의 포트폴리오입니다.",
+    images: "/og-image.png",
+  },
 };
