d6a15c56ebf7fdb08aeeed8a45d51e0e6f423d4c
KIMMINGYU99 <kmk4604@gmail.com>
2025-05-02 22:20:18 +0900

    프론트 임시

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,32 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/component/layout/header/Header";
-import Footer from "@/component/layout/footer/Footer";
-import ClientLayout from "@/component/layout/ClientLayout";
 import LoadingScreen from "@/component/loading/LoadingScreen";
 import { Suspense } from "react";
 import { LoadingProvider } from "@/context/LoadingContext";
 import MainContent from "@/component/layout/MainContent";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  title: "김민규 | 포트폴리오",
+  description: "신입 프론트엔드 개발자 김민규의 포트폴리오입니다.",
+  openGraph: {
+    title: "김민규 | 포트폴리오",
+    description: "신입 프론트엔드 개발자 김민규의 포트폴리오입니다.",
+    url: "https://final-portfolio-ruddy-five.vercel.app",
+    images: [
+      {
+        url: "/images/thumbnail.png",
+        width: 1200,
+        height: 630,
+        alt: "김민규 포트폴리오 미리보기 이미지",
+      },
+    ],
+    type: "website",
+    locale: "ko_KR",
+    siteName: "김민규 포트폴리오",
+  },
+};
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -18,19 +34,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <Head>
-        <title>김민규 | 포트폴리오</title>
-        <meta property="og:title" content="김민규 | 포트폴리오" />
-        <meta
-          property="og:description"
-          content="신입 프론트엔드 개발자 김민규의 포트폴리오입니다."
-        />
-        <meta property="og:image" content="/images/thumbnail.png" />
-        <meta
-          property="og:url"
-          content="https://final-portfolio-ruddy-five.vercel.app"
-        />
-      </Head>
       <body className={inter.className}>
         <LoadingProvider>
           <Suspense fallback={null}>
